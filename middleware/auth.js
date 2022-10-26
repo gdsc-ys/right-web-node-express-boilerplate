911b06aaead71e2ed17d237172613b60c6b986a0
jennyjunho2 <a01032208149@gmail.com>
2022-10-26 20:37:16 +0900

    Refactor : Refactor status code response

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,17 @@
 const jwt = require("../utils/token");
+const STATUS_CODE = require("../utils/status");
 
 module.exports = async (req, res, next) => {
   const cookie = req.get("cookie");
   if (!cookie) {
-    res.status(401).json({ fail: "NO_TOKEN" });
+    res.status(STATUS_CODE.Unauthorized).json({ fail: "NO_TOKEN" });
   }
   const token = cookie.split("=")[1];
   jwt.verify(token).then((decode) => {
     if (decode === "TOKEN_EXPIRED") {
-      res.status(401).json({ fail: "TOKEN_EXPIRED" });
+      res.status(STATUS_CODE.Unauthorized).json({ fail: "TOKEN_EXPIRED" });
     } else if (decode === "TOKEN_INVALID") {
-      res.status(401).json({ fail: "TOKEN_INVALID" });
+      res.status(STATUS_CODE.Unauthorized).json({ fail: "TOKEN_INVALID" });
     } else {
       console.log(decode.id, decode.email);
       req.payload = { id: decode.id, email: decode.email };
